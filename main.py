c4f2eb9a355dabb1cf28e810aee325dcbbe1e264
thaissantanna <athsantanna@gmail.com>
2025-07-11 18:46:44 -0300

    adicionando transacoes

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from api.carro_api import CarroAPI
 from api.cliente_api import ClienteAPI
+from api.transacao_api import TransacaoAPI
 
 def main():
     carro_api = CarroAPI()
     cliente_api = ClienteAPI()
+    transacao_api = TransacaoAPI()
 
     while True:
         print("\n===== MENU PRINCIPAL =====")
-        print("1. CRUD Carros")
-        print("2. CRUD Clientes")
+        print("1. Carros")
+        print("2. Clientes")
+        print("3. Transações")
         print("0. Sair")
 
         opcao = input("Escolha uma opção: ")
@@ -17,6 +20,8 @@ def main():
             carro_api.menu()
         elif opcao == "2":
             cliente_api.menu()
+        elif opcao == "3":
+            transacao_api.menu()
         elif opcao == "0":
             break
         else:
